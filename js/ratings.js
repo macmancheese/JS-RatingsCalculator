c5ce2300f1efea96c541c07a470909c317f3b136
macchristensen <macchristensen@mail.weber.edu>
2020-02-21 21:15:37 -0700

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,7 +1,7 @@
 function collect_ratings() {
     var ratings = {count: 0, sum: 0, average: 0};
-    let rating = 0;
-    const elements = document.querySelectorAll('.rating');
+    var rating = 0;
+    let elements = document.querySelectorAll('.rating');
     elements.forEach(element => {
         rating = parseInt(element.id.replace('star', ''));
         ratings.count += parseInt(element.value);
