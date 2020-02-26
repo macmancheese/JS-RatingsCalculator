76677c95ee8dc3988c6366ed6b9fe64f65d35ace
macchristensen <macchristensen@mail.weber.edu>
2020-02-25 21:22:44 -0700

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -1,5 +1,5 @@
-function collect_ratings = () => {
-    const ratings = {'count': 0, 'sum': 0, 'average': 0};
+function collect_ratings() {
+    const ratings = { 'count': 0, 'sum': 0, 'average': 0 };
     let rating = 0;
     const elements = document.querySelectorAll('.rating');
     elements.forEach(element => {
@@ -9,12 +9,12 @@ function collect_ratings = () => {
     }); 
     
     if (ratings.count !== 0) {
-        ratings.average = ratings.sum / ratings.count);
+        ratings.average = ratings.sum / ratings.count;
     }
     return ratings;
 };
 
 document.addEventListener('change', () => {
-    const ratings = collect_ratings();
-    document.querySelector('#average').value = ratings.average.toFixed(2);
+   const ratings = collect_ratings();
+   document.querySelector('#average').value = ratings.average.toFixed(2);
 });
